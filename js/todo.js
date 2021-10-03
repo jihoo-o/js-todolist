b2e69362a2672f2f0403b555f4e482fbca913aad
Liz <seonhwakei@gmail.com>
2021-10-03 22:35:18 +0900

    Update js: display todo
    separate function 'displayTodo' from function 'addTodo'

--- js/todo.js
+++ js/todo.js
@@ -3,19 +3,27 @@
 const inputForm = document.querySelector('#input__form');
 const list = document.querySelector('.list__item');
 
-function addTodo(text) {
+function displayTodo(id, text, state) {
     const newItem = document.createElement('li');
     newItem.setAttribute('class', 'list__item-box');
+    newItem.dataset.id = id;
+    newItem.dataset.state = state;
     newItem.innerHTML = `
-        <h3 class="list__item__text">${text}</h3>
-        <button class="list__item__btn-check">
-            <i class="far fa-check-circle"></i>
-        </button>
-        <button class="list__item__btn-delete">
-            <i class="far fa-trash-alt"></i>
-        </button>
+    <h3 class="list__item__text">${text}</h3>
+    <button class="list__item__btn-check">
+    <i class="far fa-check-circle"></i>
+    </button>
+    <button class="list__item__btn-delete">
+    <i class="far fa-trash-alt"></i>
+    </button>
     `;
     list.prepend(newItem);
+}
+
+function addTodo(text) {
+    const id = Date.now() * Math.floor(Math.random() * 100);
+    const UNCHECKED = 'unchecked';
+    displayTodo(id, text, UNCHECKED);
 }
 
 inputForm.addEventListener('submit', (e) => {
